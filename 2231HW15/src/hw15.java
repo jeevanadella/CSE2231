a18deda85f14f5916e0acb2c105b67dc076c60dd
Jeevan Nadella <100245897+jeevanadella@users.noreply.github.com>
2023-09-24 18:30:08 -0400

    Add files via upload

---
+++ 2231HW15/src/hw15.java
@@ -0,0 +1,101 @@
+import java.util.Comparator;
+
+import components.queue.Queue;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class hw15 {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private hw15() {
+    }
+
+    /**
+     * Inserts the given {@code T} in the {@code Queue<T>} sorted according to
+     * the given {@code Comparator<T>} and maintains the {@code Queue<T>}
+     * sorted.
+     *
+     * @param <T>
+     *            type of {@code Queue} entries
+     * @param q
+     *            the {@code Queue} to insert into
+     * @param x
+     *            the {@code T} to insert
+     * @param order
+     *            the {@code Comparator} defining the order for {@code T}
+     * @updates q
+     * @requires <pre>
+     * IS_TOTAL_PREORDER([relation computed by order.compare method])  and
+     * IS_SORTED(q, [relation computed by order.compare method])
+     * </pre>
+     * @ensures <pre>
+     * perms(q, #q * <x>)  and
+     * IS_SORTED(q, [relation computed by order.compare method])
+     * </pre>
+     */
+    private static <T> void insertInOrder(Queue<T> q, T x,
+            Comparator<T> order) {
+
+        q.enqueue(x);
+
+        Queue<T> newQueue = q.newInstance();
+
+        while (q.length() != 0 && order.compare(x, q.front()) > 0) {
+            newQueue.enqueue(q.dequeue());
+        }
+
+        newQueue.enqueue(x);
+        newQueue.append(q);
+
+        q.transferFrom(newQueue);
+    }
+
+    /**
+     * Sorts {@code this} according to the ordering provided by the
+     * {@code compare} method from {@code order}.
+     *
+     * @param order
+     *            ordering by which to sort
+     * @updates this
+     * @requires IS_TOTAL_PREORDER([relation computed by order.compare method])
+     * @ensures <pre>
+     * perms(this, #this)  and
+     * IS_SORTED(this, [relation computed by order.compare method])
+     * </pre>
+     */
+    public void sort(Comparator<T> order) {
+
+        Queue<T> newQueue = this.newInstance();
+
+        while (this.length() != 0) {
+            insertInOrder(newQueue, this.dequeue(), order);
+        }
+
+        this.transferFrom(newQueue);
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+
+        in.close();
+        out.close();
+    }
+
+}
